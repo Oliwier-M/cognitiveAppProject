0da3ba74ddff45b0983961e30669cc1bf497588a
Julia Galant <276068@student.pwr.edu.pl>
2024-06-24 22:04:25 +0200

    user saving to firebase realtime db

---
+++ app/src/main/java/com/example/cognitiveassesmenttest/ui/db/User.kt
@@ -0,0 +1,10 @@
+package com.example.cognitiveassesmenttest.ui.db
+
+class User (
+    val userId : String,
+    val name: String,
+    val surname: String,
+    val email: String,
+    val username: String,
+
+)
